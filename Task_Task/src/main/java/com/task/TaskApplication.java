9e3c5008017c257d7756dc2fd2a37ddb9e2942da
lucky <lucky@192.168.1.72>
2023-07-13 13:40:54 +0530

    UpdatedTask

--- Task_Task/src/main/java/com/task/TaskApplication.java
+++ Task_Task/src/main/java/com/task/TaskApplication.java
@@ -9,6 +9,16 @@ public class TaskApplication
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
+		
+		/*URL's of BackEnd Services
+		 * 
+		 *Insert Data= 		 	http://localhost:2000
+		 *Retrieving Data:   	http://localhost:2000/getAll
+		 *Finding Average:   	http://localhost:2000/average
+		 *Finding Middle No: 	http://localhost:2000/middle
+		 *Finding Duplicates:	http://localhost:2000/duplicates
+		 *Finding even-odd-averages: http://localhost:2000/even-odd-averages
+		 */
 	}
 
 }
